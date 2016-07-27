03f3506aa5f5e05f005ab1590874facfdb2be41e
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-07-27 12:57:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+  module.exports = function () {
+    const self = this
+    const length = this.length
+    let i = 0
+    let context
+    let nextFunc
+
+    return {
+      next (c, n) {
+        if (!context) context = c
+        if (!nextFunc) nextFunc = n
+
+        let fn = self[i++] || nextFunc
+        let done = i > length
+        let value
+        let nextCalled = false
+
+        if (fn) {
+          value = fn(context, () => {
+            if (nextCalled) {
+              return Promise.reject(new Error('next() called multiple times'))
+            }
+            nextCalled = true
+            return Promise.resolve(this.next().value)
+          })
+        }
+
+        return {
+          value,
+          done
+        }
+      }
+    }
+  }
